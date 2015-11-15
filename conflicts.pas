2035bcbde3856aa8b551d0cbb6fb86634cdee201
Nicholas <ruscoolman@gmail.com>
2015-11-15 14:32:21 +1000

    init conflict class

--- conflicts.pas
+++ conflicts.pas
@@ -7,7 +7,27 @@ interface
 uses
   Classes, SysUtils;
 
+type
+
+  TConflict = class;
+  TConflictClass = class of TConflict;
+
+  TConflict = class
+  private
+    RecordID     : integer;
+    ConflictType : TConflictClass;
+  public
+    constructor Create(ARecordID : integer; AConflictType : TConflictClass);
+  end;
+
 implementation
+
+
+constructor TConflict.Create(ARecordID : integer; AConflictType : TConflictClass);
+begin
+  RecordID     := ARecordID;
+  ConflictType := AconflictType;
+end;
 
 end.
 
